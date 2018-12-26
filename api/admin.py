1053c13f12b0c4a5427f511867540c864e78336e
Takayuki-Yamamoto <yamamoto@lifematics.co.jp>
2018-12-27 01:30:12 +0900

    bugfix

--- api/admin.py
+++ api/admin.py
@@ -4,8 +4,8 @@ from . import models as md
 
 
 class PhotoUrlModelAdmin(admin.ModelAdmin):
-    list_display = ('id', 'uri' 'created_at',)
-    ordering = ('id')
+    list_display = ('id', 'uri', 'created_at',)
+    ordering = ('id',)
 
 
 admin.site.register(md.PhotoUrl, PhotoUrlModelAdmin)
