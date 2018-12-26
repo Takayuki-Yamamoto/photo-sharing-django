80135dbb0c67fa93ca8d22de6d1dce0a1fd36714
Takayuki-Yamamoto <yamamoto@lifematics.co.jp>
2018-12-27 01:42:07 +0900

    Update model

--- api/models.py
+++ api/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+from datetime import datetime
 
 class PhotoUrl(models.Model):
     url = models.URLField()
-    created_at = models.DateTimeField(auto_created=True)
+    created_at = models.DateTimeField(default=datetime.now)
 
     def __str__(self):
         return self.url
