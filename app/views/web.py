7d6d6cf548f1043d25e8c465f45195d4259f44ff
Takayuki-Yamamoto <yamamoto@lifematics.co.jp>
2018-12-29 21:26:15 +0900

    Remove redundant function. Fix template.

--- app/views/web.py
+++ app/views/web.py
@@ -6,6 +6,3 @@ from app.models import PhotoUrl
 class PhotoListView(ListView):
     model = PhotoUrl
     template_name = 'html/app/index.html'
-
-    # def get_context_data(self, *, object_list=None, **kwargs):
-    #     return super().get_context_data()
