620f08f08123c2ea375ccc76dd5a38b1d8b64c3a
Takayuki-Yamamoto <yamamoto@lifematics.co.jp>
2019-01-18 01:25:56 +0900

    tmp

--- app/views/web.py
+++ app/views/web.py
@@ -9,6 +9,12 @@ class TimeLineView(LoginRequiredMixin, ListView):
     template_name = 'app/timeline.html'
     queryset = Photo.objects.order_by('created_at').reverse().all()
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(TimeLineView, self).get_context_data(**kwargs)
+        print("*****************************************")
+        print(dict(context))
+        return context
+
 
 class SlideShowView(LoginRequiredMixin, ListView):
     model = Photo
